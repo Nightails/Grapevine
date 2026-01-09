53e18b732eadad02b5c29556f411356845fb8f92
a_nightails <a.nightails@gmail.com>
2026-01-08 19:19:25 -0600

    Add SQLC configuration, user query definitions, and adjust .gitignore

---
+++ database/queries/users.sql
@@ -0,0 +1,16 @@
+-- name: AddUser :one
+INSERT INTO users (id, created_at, updated_at, password)
+VALUES (?1, ?2, ?3, ?4)
+RETURNING *;
+
+-- name: GetUserByID :one
+SELECT * FROM users WHERE id = ?1;
+
+-- name: UpdateUserByID :exec
+UPDATE users SET password = ?2 WHERE id = ?1;
+
+-- name: RemoveUserByID :exec
+DELETE FROM users WHERE id = ?1;
+
+-- name: RemoveAllUsers :exec
+DELETE FROM users;
