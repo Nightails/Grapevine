5784c8a8076e69be42419fbca250f38c67c38b2f
Alan Night <a.nightails@gmail.com>
2026-01-17 12:52:50 -0600

    Add `merchant_id` field to transactions schema and update related queries

--- database/migrations/003_transactions.sql
+++ database/migrations/003_transactions.sql
@@ -5,13 +5,15 @@ CREATE TABLE transactions (
     updated_at TEXT NOT NULL,
     user_id TEXT NOT NULL,
     account_id TEXT NOT NULL,
+    merchant_id TEXT NOT NULL,
     amount INTEGER NOT NULL DEFAULT 0,
     currency TEXT NOT NULL DEFAULT 'USD',
     description TEXT NOT NULL DEFAULT '',
     status TEXT NOT NULL DEFAULT 'pending',
     date TEXT NOT NULL DEFAULT CURRENT_DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (merchant_id) REFERENCES merchants(id) ON DELETE CASCADE
 );
 
 -- +goose Down
--- database/queries/transactions.sql
+++ database/queries/transactions.sql
@@ -1,6 +1,6 @@
 -- name: AddTransaction :one
-INSERT INTO transactions (id, created_at, updated_at, user_id, account_id, amount, currency, description, status, date)
-VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)
+INSERT INTO transactions (id, created_at, updated_at, user_id, account_id, merchant_id, amount, currency, description, status, date)
+VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11)
 RETURNING *;
 
 -- name: GetAllTransactions :many
