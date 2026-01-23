a6296d4a1e93f7731763fba89ee3de5e034c1e4d
Alan Night <a.nightails@gmail.com>
2026-01-23 11:18:37 -0600

    Enable command execution in CLI; handle unknown commands with error response

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"budgetcli/internal/config"
 	"budgetcli/internal/database"
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -23,8 +24,8 @@ type commands struct {
 }
 
 func Run(cfg *config.Config, db *sql.DB) {
-	//s := state{dbQueries: database.New(db), cfg: cfg}
-	//cmds := commands{cmdMap: make(map[string]func(*state, command) error)}
+	s := state{dbQueries: database.New(db), cfg: cfg}
+	cmds := commands{cmdMap: make(map[string]func(*state, command) error)}
 
 	// Parse arguments
 	args := os.Args
@@ -33,4 +34,19 @@ func Run(cfg *config.Config, db *sql.DB) {
 		printHelp()
 		os.Exit(1)
 	}
+
+	// Run command
+	cmd := command{name: args[1], args: args[2:]}
+	if err := cmds.run(&s, cmd); err != nil {
+		fmt.Println("Error running command:", err)
+		os.Exit(1)
+	}
+}
+
+func (cmds *commands) run(s *state, c command) error {
+	fn, ok := cmds.cmdMap[c.name]
+	if !ok {
+		return errors.New("unknown command")
+	}
+	return fn(s, c)
 }
